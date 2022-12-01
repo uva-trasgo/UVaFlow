0feffced9d9c968b54a4e3635856ef5c6ecce00a
Rocío Carratalá-Sáez <rocio@infor.uva.es>
2022-12-01 02:21:44 +0100

    Adjust in read functions headers

--- include/read_python_files.h
+++ include/read_python_files.h
@@ -14,7 +14,7 @@ void read_faces ( char *filename, int nDim, int nVertsPerFace, int nFaces, int *
 /* Supported dimensions: 2D, 3D */
 /* NOTE: 1st line of given file must contain mesh->nTimes */
 /* OUT: mesh->times and mesh->nTimes set */
-void read_time ( char *filename, int nTimes, double *times );
+void read_times ( char *filename, int nTimes, double *times );
 
 /* Function to set all mesh points' velocity read from file "filename" */
 /* Supported dimensions: 2D, 3D */
